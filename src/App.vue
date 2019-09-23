3ef8b29e1965266cabaffeaaec2f47bf18aa08d4
kaduki <1353377@gmail.com>
2019-09-23 18:04:44 +0900

    remove async load

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,31 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
+      <!-- <HelloWorld/> -->
+
+      <router-link to="/home">Go to home</router-link>
+      <router-link to="/add">Go to add</router-link>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    HelloWorld
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
